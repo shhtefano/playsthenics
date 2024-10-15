3e0b0d820af90c7f8ece2222ec985e670d9633f5
shhtefano <sfngln00@gmail.com>
2024-10-16 00:20:30 +0200

    Added sqlite db with mockup data

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,7 +1,7 @@
 import sqlite3 from 'sqlite3';
 
 // Inizializza il database SQLite
-export const db = new sqlite3.Database('../server/db/db.sqlite', (err: Error | null) => {
+export const db = new sqlite3.Database('../server/db/database.sqlite', (err: Error | null) => {
   if (err) {
     throw err;
   }
